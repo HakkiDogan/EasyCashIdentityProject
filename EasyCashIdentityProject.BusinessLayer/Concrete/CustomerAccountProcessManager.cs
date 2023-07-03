894796b37fe06e3d6bbf5f297abe2f9a7ec739f8
HakkiDogan <180316029@ogr.cbu.edu.tr>
2023-07-03 15:23:17 +0300

    Generic class oluşturuldu. Business katmanına service ve manager sınıfları eklendi.

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,46 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountProcessManager : ICustomerAccountProcessService
+    {
+        private readonly ICustomerAccountProcessDal _customerAccountProcessDal;
+
+        public CustomerAccountProcessManager(ICustomerAccountProcessDal customerAccountProcessDal)
+        {
+            _customerAccountProcessDal = customerAccountProcessDal;
+        }
+
+        public void Delete(CustomerAccountProcess entity)
+        {
+            _customerAccountProcessDal.Delete(entity);
+        }
+
+        public CustomerAccountProcess GetById(int id)
+        {
+            return _customerAccountProcessDal.GetById(id);
+        }
+
+        public List<CustomerAccountProcess> GetList()
+        {
+            return _customerAccountProcessDal.GetList();
+        }
+
+        public void Insert(CustomerAccountProcess entity)
+        {
+            _customerAccountProcessDal.Insert(entity);
+        }
+
+        public void Update(CustomerAccountProcess entity)
+        {
+            _customerAccountProcessDal.Update(entity);
+        }
+    }
+}
