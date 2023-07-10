1d14f687784094b032199dc94e0409ba56ee4482
HakkiDogan <180316029@ogr.cbu.edu.tr>
2023-07-10 18:20:01 +0300

    Mail doğrulama kodu doğrulaması eklendi

--- EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -1,19 +1,38 @@
-﻿using EasyCashIdentityProject.PresentationLayer.Models;
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using EasyCashIdentityProject.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
 	public class ConfirmMailController : Controller
 	{
-		[HttpGet]
-		public IActionResult Index(int id)
+		private readonly UserManager<AppUser> _userManager;
+
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+		public async Task<IActionResult> Index(int id)
 		{
-			return View();
+			var value = TempData["Mail"];
+			TempData.Keep("Mail");
+            ViewBag.v = value;
+            return View();
 		}
 
 		[HttpPost]
-        public IActionResult Index(ConfirmMailViewModel confirmMailViewModel)
+        public async Task<IActionResult> Index(ConfirmMailViewModel confirmMailViewModel)
         {
+            var value = TempData["Mail"];
+            TempData.Keep("Mail");
+            var user = await _userManager.FindByEmailAsync(value.ToString());
+            if(user.ConfirmCode == confirmMailViewModel.ConfirmCode)
+            {
+                return RedirectToAction("Index","MyProfile");
+            }
             return View();
         }
     }
--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -67,7 +67,6 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
 
                     
                     TempData["Mail"] = appUserRegisterDto.Email;
-                    TempData.Keep("Mail");
 
                     return RedirectToAction("Index","ConfirmMail");
                 }
