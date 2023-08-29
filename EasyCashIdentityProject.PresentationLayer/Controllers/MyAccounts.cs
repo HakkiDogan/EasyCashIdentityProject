657cd58838f6edd2ef6b4290c2cd0afc03871437
HakkiDogan <180316029@ogr.cbu.edu.tr>
2023-08-29 13:43:39 +0300

    Profil bilgileri güncelleme ekranı yapıldı.

--- EasyCashIdentityProject.PresentationLayer/Controllers/MyAccounts.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MyAccounts.cs
@@ -1,12 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
+	[Authorize]
 	public class MyAccounts : Controller
 	{
-		public IActionResult Index()
+		private readonly UserManager<AppUser> _userManager;
+
+        public MyAccounts(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+		[HttpGet]
+        public async Task<IActionResult> Index()
 		{
+			var values = await _userManager.FindByNameAsync(User.Identity.Name);
+			AppUserUpdateDto appUserUpdateDto = new();
+			appUserUpdateDto.Name = values.Name;
+			appUserUpdateDto.Surname = values.Surname;
+			appUserUpdateDto.PhoneNumber = values.PhoneNumber;
+			appUserUpdateDto.Email = values.Email;
+			appUserUpdateDto.City = values.City;
+			appUserUpdateDto.District = values.District;
+			appUserUpdateDto.ImageUrl = values.ImageUrl;
+			return View(appUserUpdateDto);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(AppUserUpdateDto appUserUpdateDto)
+		{
+			if (appUserUpdateDto.Password == appUserUpdateDto.ConfirmPassword)
+			{
+				var user = await _userManager.FindByNameAsync(User.Identity.Name);
+				user.PhoneNumber = appUserUpdateDto.PhoneNumber;
+				user.Name = appUserUpdateDto.Name;
+				user.Surname = appUserUpdateDto.Surname;
+				user.City = appUserUpdateDto.City;
+				user.District = appUserUpdateDto.District;
+				user.ImageUrl = "test";
+				user.Email = appUserUpdateDto.Email;
+				user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, appUserUpdateDto.Password);
+				var result = await _userManager.UpdateAsync(user);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Index", "Login");
+				}
+			}
+
 			return View();
+
 		}
 	}
 }
